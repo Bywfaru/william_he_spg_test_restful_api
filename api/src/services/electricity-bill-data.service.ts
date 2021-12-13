c737f61433060298724b7039cb24593260885507
William He <contact@williamhe.dev>
2021-12-12 20:57:59 -0800

    Initial commit

---
+++ api/src/services/electricity-bill-data.service.ts
@@ -0,0 +1,27 @@
+import {/* inject, */ BindingScope, injectable} from '@loopback/core';
+import {Filter} from '@loopback/repository';
+import {ElectricityConsumption} from '../models';
+import csvtojson from 'csvtojson';
+
+@injectable({scope: BindingScope.TRANSIENT})
+export class ElectricityBillDataService {
+  constructor(/* Add @inject to inject parameters */) {}
+
+  private ELECTRICITY_BILL_DATA_PATH = './data/electricity_bill_data.csv';
+
+  async getAll(
+    filter?: Filter<ElectricityConsumption>,
+  ): Promise<Array<ElectricityConsumption>> {
+    const response: ElectricityConsumption[] = [];
+
+    let filterTracker: Filter<ElectricityConsumption>;
+
+    if (filter) {
+      filterTracker = JSON.parse(JSON.stringify(filter));
+    }
+    
+    await csvtojson().fromFile(this.ELECTRICITY_BILL_DATA_PATH);
+
+    return response;
+  }
+}
