4cfafbcd6b53c287a76c449f6e01cfad97aab292
William He <contact@williamhe.dev>
2021-12-30 02:55:19 -0800

    Refactoring

--- client/src/views/home/functions.ts
+++ client/src/views/home/functions.ts
@@ -79,6 +79,11 @@ async function billDataRequest(utilityType: UtilityType) {
     }
 }
 
+/**
+ * Retrieves all the different bill data
+ * 
+ * @returns object of arrays of objects
+ */
 export async function getBillData() {
     try {
         const electricityBillData: ElectricityBillData[] = await billDataRequest(UtilityType.ELECTRICITY) as ElectricityBillData[];
@@ -100,57 +105,59 @@ export async function getBillData() {
 
 interface ElectricityKwhData {
     kwh: number,
-        date: Date
+    date: Date
 }
 
 interface WaterM3Data {
     m3: number,
-        date: Date
+    date: Date
 }
 
 interface GasGjData {
     gj: number,
-        date: Date
+    date: Date
 }
 
-export function generateElectricityLineChart(svgElement: SVGSVGElement, billData: BillData, fromDate: Date, toDate: Date, clientWidth: number) {
+function buildLineChart(
+    svgElement: SVGSVGElement,
+    fromDate: Date,
+    toDate: Date,
+    clientWidth: number,
+    consumptionData: ElectricityKwhData[] | GasGjData[] | WaterM3Data[],
+    yValue: (d: any) => number,
+    maxConsumption: number,
+    titleLabel: string,
+    yAxisLabel: string,
+) {
     const margin = {
         top: 30,
         right: 85,
         bottom: 50,
         left: 85,
     };
-
-    const xValue = (d: any) => d.date;
-    const yValue = (d: any) => d.kwh as number;
     const compareTime = (a: Date, b: Date) => a.getTime() - b.getTime();
 
-    const electricityKwhData: ElectricityKwhData[] =
-        billData.electricityBillData.map((data) => ({
-                kwh: +data.k_wh_consumption,
-                date: new Date(+data.year, +data.month),
-            } as ElectricityKwhData)
-        );
+    const xValue = (d: any) => d.date;
+
     const minDate: Date = fromDate || d3.min(
-        electricityKwhData,
-        (d) => d.date as Date
+        consumptionData as any,
+        (d: ElectricityKwhData | WaterM3Data | GasGjData) => d.date as Date
     ) as Date;
     const maxDate: Date = toDate || d3.max(
-        electricityKwhData,
-        (d) => d.date as Date
+        consumptionData as any,
+        (d: any) => d.date as Date
     ) as Date;
-    const electricityMaxKwh: number = d3.max(
-        electricityKwhData,
-        (d) => d.kwh
-    ) as number;
-    electricityKwhData.sort((a: ElectricityKwhData, b: ElectricityKwhData) =>
+
+    consumptionData.sort((a: ElectricityKwhData | WaterM3Data | GasGjData, b: ElectricityKwhData | WaterM3Data | GasGjData) =>
         compareTime(a.date, b.date)
     );
 
     const width = clientWidth;
-    const height = electricityKwhData.length;
+    const height = consumptionData.length;
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
+
+    console.log(innerWidth);
 
     const svg = d3
         .select(svgElement)
@@ -166,7 +173,7 @@ export function generateElectricityLineChart(svgElement: SVGSVGElement, billData
         .nice();
     const yScale = d3
         .scaleLinear()
-        .domain([0, electricityMaxKwh])
+        .domain([0, maxConsumption])
         .range([innerHeight, 0])
         .nice();
 
@@ -191,7 +198,7 @@ export function generateElectricityLineChart(svgElement: SVGSVGElement, billData
         .attr("fill", "black")
         .attr("transform", `rotate(-90)`)
         .attr("text-anchor", "middle")
-        .text("Consumption (kwh)");
+        .text(('Consumption ' + yAxisLabel));
     const xAxisG = g
         .append("g")
         .call(xAxis)
@@ -205,11 +212,11 @@ export function generateElectricityLineChart(svgElement: SVGSVGElement, billData
 
     g.append("path")
         .attr("class", "line-path")
-        .attr("d", lineGenerator(electricityKwhData as any))
+        .attr("d", lineGenerator(consumptionData as any))
         .attr("fill", "none")
         .attr("stroke", "green");
     g.selectAll("circle")
-        .data(electricityKwhData)
+        .data(consumptionData as any)
         .enter()
         .append("circle")
         .attr("cx", (d) => xScale(xValue(d)))
@@ -220,22 +227,57 @@ export function generateElectricityLineChart(svgElement: SVGSVGElement, billData
         .attr("class", "title")
         .attr("y", -10)
         .attr("x", innerWidth / 2 - margin.left / 2)
-        .text("Electricity Bill Data");
+        .text(titleLabel);
 
     d3.select(".tick line").attr("stroke", "#d1e8ff");
 }
 
-export function generateWaterLineChart(svgElement: SVGSVGElement, billData: BillData, fromDate: Date, toDate: Date, clientWidth: number) {
-    const margin = {
-        top: 30,
-        right: 85,
-        bottom: 50,
-        left: 85,
-    };
+/**
+ * Generates the requested line chart
+ * 
+ * @param svgElement SVGSVGElement DOM element
+ * @param billData BillData object
+ * @param fromDate Date object representing the lower range
+ * @param toDate Date object representing the upper range
+ * @param clientWidth the width of the container
+ */
+export function generateElectricityLineChart(svgElement: SVGSVGElement, billData: BillData, fromDate: Date, toDate: Date, clientWidth: number) {
+    const yValue = (d: any) => d.kwh as number;
+    
+    const electricityKwhData: ElectricityKwhData[] =
+    billData.electricityBillData.map((data) => ({
+        kwh: +data.k_wh_consumption,
+        date: new Date(+data.year, +data.month),
+    } as ElectricityKwhData));
 
-    const xValue = (d: any) => d.date;
+    const electricityMaxKwh: number = d3.max(
+        electricityKwhData,
+        (d) => d.kwh
+    ) as number;
+
+    const titleLabel = 'Electricity Bill Data';
+    const xAxisLabel = '(kwh)';
+
+    buildLineChart(svgElement, fromDate, toDate, clientWidth, electricityKwhData, yValue, electricityMaxKwh, titleLabel, xAxisLabel);
+}
+
+/**
+ * Generates the requested line chart
+ * 
+ * @param svgElement SVGSVGElement DOM element
+ * @param billData BillData object
+ * @param fromDate Date object representing the lower range
+ * @param toDate Date object representing the upper range
+ * @param clientWidth the width of the container
+ */
+export function generateWaterLineChart(
+    svgElement: SVGSVGElement, 
+    billData: BillData, 
+    fromDate: Date, 
+    toDate: Date, 
+    clientWidth: number
+) {
     const yValue = (d: any) => d.m3 as number;
-    const compareTime = (a: Date, b: Date) => a.getTime() - b.getTime();
 
     const waterM3Data: WaterM3Data[] =
         billData.waterBillData.map((data) => ({
@@ -243,113 +285,34 @@ export function generateWaterLineChart(svgElement: SVGSVGElement, billData: Bill
                 date: new Date(+data.year, +data.month),
             } as WaterM3Data)
         );
-    const minDate: Date = fromDate || d3.min(
-        waterM3Data,
-        (d) => d.date as Date
-    ) as Date;
-    const maxDate: Date = toDate || d3.max(
-        waterM3Data,
-        (d) => d.date as Date
-    ) as Date;
     const waterMaxM3: number = d3.max(
         waterM3Data,
         (d) => d.m3
     ) as number;
-    waterM3Data.sort((a: WaterM3Data, b: WaterM3Data) =>
-        compareTime(a.date, b.date)
-    );
 
-    console.log(waterM3Data);
-
-    const width = clientWidth;
-    const height = waterM3Data.length;
-    const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
-
-    const svg = d3
-        .select(svgElement)
-        .attr("width", width)
-        .attr("height", height)
-        .attr("background", "#d3d3d3");
-    svg.selectAll('g > *').remove();
-
-    const xScale = d3
-        .scaleTime()
-        .domain([minDate, maxDate])
-        .range([0, innerWidth])
-        .nice();
-    const yScale = d3
-        .scaleLinear()
-        .domain([0, waterMaxM3])
-        .range([innerHeight, 0])
-        .nice();
-
-    const g = svg
-        .append("g")
-        .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-    const lineGenerator = d3
-        .line()
-        .x((d) => xScale(xValue(d)))
-        .y((d) => yScale(yValue(d)));
-
-    const yAxis = d3.axisLeft(yScale).tickSize(-innerWidth);
-    const xAxis = d3.axisBottom(xScale);
-
-    const yAxisG = g.append("g").call(yAxis).style("font-size", "1em");
-    yAxisG
-        .append("text")
-        .attr("class", "axis-label")
-        .attr("y", -70)
-        .attr("x", -innerHeight / 2)
-        .attr("fill", "black")
-        .attr("transform", `rotate(-90)`)
-        .attr("text-anchor", "middle")
-        .html("Consumption (&#13221)");
-    const xAxisG = g
-        .append("g")
-        .call(xAxis)
-        .attr("transform", `translate(0, ${innerHeight})`)
-        .style("font-size", "1em");
-    xAxisG
-        .selectAll("text")
-        .attr("x", -30)
-        .attr("transform", "rotate(-45)")
-        .style("text-anchor", "start");
-
-    g.append("path")
-        .attr("class", "line-path")
-        .attr("d", lineGenerator(waterM3Data as any))
-        .attr("fill", "none")
-        .attr("stroke", "green");
-    g.selectAll("circle")
-        .data(waterM3Data)
-        .enter()
-        .append("circle")
-        .attr("cx", (d) => xScale(xValue(d)))
-        .attr("cy", (d) => yScale(yValue(d)))
-        .attr("r", 3)
-        .style("fill", "green");
-    g.append("text")
-        .attr("class", "title")
-        .attr("y", -10)
-        .attr("x", innerWidth / 2 - margin.left / 2)
-        .text("Water Bill Data");
-
-    d3.select(".tick line").attr("stroke", "#d1e8ff");
+    const titleLabel = 'Water Bill Data';
+    const xAxisLabel = '(meters cubed)';
+    
+    buildLineChart(svgElement, fromDate, toDate, clientWidth, waterM3Data, yValue, waterMaxM3, titleLabel, xAxisLabel);
 }
 
-export function generateGasLineChart(svgElement: SVGSVGElement, billData: BillData, fromDate: Date, toDate: Date, clientWidth: number) {
-    const margin = {
-        top: 30,
-        right: 85,
-        bottom: 50,
-        left: 85,
-    };
-
-    const xValue = (d: any) => d.date;
+/**
+ * Generates the requested line chart
+ * 
+ * @param svgElement SVGSVGElement DOM element
+ * @param billData BillData object
+ * @param fromDate Date object representing the lower range
+ * @param toDate Date object representing the upper range
+ * @param clientWidth the width of the container
+ */
+export function generateGasLineChart(
+    svgElement: SVGSVGElement, 
+    billData: BillData, 
+    fromDate: Date, 
+    toDate: Date, 
+    clientWidth: number
+) {
     const yValue = (d: any) => d.gj as number;
-    const compareTime = (a: Date, b: Date) => a.getTime() - b.getTime();
 
     const gasGjData: GasGjData[] =
         billData.gasBillData.map((data) => ({
@@ -357,97 +320,26 @@ export function generateGasLineChart(svgElement: SVGSVGElement, billData: BillDa
                 date: new Date(+data.year, +data.month),
             } as GasGjData)
         );
-    const minDate: Date = fromDate || d3.min(
-        gasGjData,
-        (d) => d.date as Date
-    ) as Date;
-    const maxDate: Date = toDate || d3.max(
-        gasGjData,
-        (d) => d.date as Date
-    ) as Date;
     const gasMaxGj: number = d3.max(
         gasGjData,
         (d) => d.gj
     ) as number;
-    gasGjData.sort((a: GasGjData, b: GasGjData) =>
-        compareTime(a.date, b.date)
-    );
 
-    const width = clientWidth;
-    const height = gasGjData.length;
-    const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
-
-    const svg = d3
-        .select(svgElement)
-        .attr("width", width)
-        .attr("height", height)
-        .attr("background", "#d3d3d3");
-    svg.selectAll('g > *').remove();
-
-    const xScale = d3
-        .scaleTime()
-        .domain([minDate, maxDate])
-        .range([0, innerWidth])
-        .nice();
-    const yScale = d3
-        .scaleLinear()
-        .domain([0, gasMaxGj])
-        .range([innerHeight, 0])
-        .nice();
-
-    const g = svg
-        .append("g")
-        .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-    const lineGenerator = d3
-        .line()
-        .x((d) => xScale(xValue(d)))
-        .y((d) => yScale(yValue(d)));
-
-    const yAxis = d3.axisLeft(yScale).tickSize(-innerWidth);
-    const xAxis = d3.axisBottom(xScale);
-
-    const yAxisG = g.append("g").call(yAxis).style("font-size", "1em");
-    yAxisG.append("text")
-        .attr("class", "axis-label")
-        .attr("y", -70)
-        .attr("x", -innerHeight / 2)
-        .attr("fill", "black")
-        .attr("transform", `rotate(-90)`)
-        .attr("text-anchor", "middle")
-        .text("Consumption (GJ)");
-    const xAxisG = g.append("g")
-        .call(xAxis)
-        .attr("transform", `translate(0, ${innerHeight})`)
-        .style("font-size", "1em");
-    xAxisG.selectAll("text")
-        .attr("x", -30)
-        .attr("transform", "rotate(-45)")
-        .style("text-anchor", "start");
-
-    g.append("path")
-        .attr("class", "line-path")
-        .attr("d", lineGenerator(gasGjData as any))
-        .attr("fill", "none")
-        .attr("stroke", "green");
-    g.selectAll("circle")
-        .data(gasGjData)
-        .enter()
-        .append("circle")
-        .attr("cx", (d) => xScale(xValue(d)))
-        .attr("cy", (d) => yScale(yValue(d)))
-        .attr("r", 3)
-        .style("fill", "green");
-    g.append("text")
-        .attr("class", "title")
-        .attr("y", -10)
-        .attr("x", innerWidth / 2 - margin.left / 2)
-        .text("Gas Bill Data");
-
-    d3.select(".tick line").attr("stroke", "#d1e8ff");
+    const titleLabel = 'Gas Bill Data';
+    const xAxisLabel = '(GJ)';
+    
+    buildLineChart(svgElement, fromDate, toDate, clientWidth, gasGjData, yValue, gasMaxGj, titleLabel, xAxisLabel);
 }
 
+/**
+ * Generates the requested line chart
+ * 
+ * @param svgElement SVGSVGElement DOM element
+ * @param billData BillData object
+ * @param fromDate Date object representing the lower range
+ * @param toDate Date object representing the upper range
+ * @param clientWidth the width of the container
+ */
 export function generateLineChart(
     svgElement: SVGSVGElement,
     billData: BillData,
